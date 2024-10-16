a66b5abd731335c9ce783927deccc1a9334079ec
Sgt-Rodrigo <rod_raven@hotmail.com>
2024-10-16 10:46:12 -0300

    feat: adds currency data fetching for dropdown menus feat: adds convert and swap buttons + working logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import InputBox from './components/InputBox/InputBox'
+import OutputBox from  './components/InputBox/InputBox';
+import useCurrencyData from './hooks/useCurrencyData';
 
 
 function App() {
@@ -8,9 +10,62 @@ function App() {
   const [from, setFrom ] = useState('usd');
   const [to, setTo] = useState('ars');
   const [convertedAmount, setConvertedAmount] = useState(0);
+  
+  //currency options
+  const currencyData = useCurrencyData(from);
+  console.log(currencyData);
+  const currencies = Object.keys(currencyData);
+  console.log(currencies)
+
+  //convertion
+  function convert(){
+    setConvertedAmount(amount * currencyData[to])
+  }
+
+  //swap
+  const swap = ()=> {
+    setFrom(to);
+    setTo(from);
+    // here we can also swap the amounts that are already showing on screen or reset them, lets swap them too >
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  }
+
 
   return (
     <>
+      <InputBox 
+        label='From'
+        amount={amount}
+        selectedCurrency={from}
+        currencyOptions={currencies}
+        onAmountChange={(amount)=> setAmount(amount)}
+        onCurrencyChange={(currency)=> setFrom(currency)}
+        amountDisabled = {false}
+        currencyDisabled = {false}
+
+      />
+
+      <OutputBox
+        label='To'
+        amount={convertedAmount}
+        selectedCurrency={to}
+        currencyOptions={currencies}
+        onAmountChange={(amount)=> setConvertedAmount(amount)}
+        onCurrencyChange={(currency)=> setTo(currency)}
+        amountDisabled = {true}
+        currencyDisabled = {false}
+      />
+
+      <button 
+        className='bg-blue-600 p-2 rounded-md text-white'
+        onClick={convert}
+      >convert</button>
+
+      <button
+        className='bg-yellow-600 p-2 rounded-md text-black'
+        onClick={swap}
+      >Swap</button>
     </>
   )
 }
--- src/hooks/useCurrencyData.ts
+++ src/hooks/useCurrencyData.ts
@@ -1,11 +1,18 @@
 import { useEffect, useState } from "react";
 
+//* index signature > to type the returned value
+//* if not done here, on the definition, you can do it in the function call. Typescript might not infer the type of the fetched data.  
+type CurrencyData = {
+    [key:string] : number
+}
 
-function useCurrencyData(currency:string) {
+
+function useCurrencyData(currency:string):CurrencyData {
     const [data, setData] = useState({});
     
-    const url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`;
+    // const url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`;
 
+    const url = `https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`
 
     useEffect(()=>{
         fetch(url)
